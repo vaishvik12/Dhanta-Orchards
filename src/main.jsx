7d3fa506ce74d6824744b341c72b46d6e122b1aa
vaishvik12 <www.vaishvikjamalta@gmail.com>
2026-06-02 16:21:34 +0530

    made scrollanimation smooth

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 import './styles/global.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
+  </React.StrictMode>
 );
+
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dhanta-orchards/',
   plugins: [react()],
+    base: '/Dhanta-Orchards/',
   server: {
     port: 5173,
     proxy: {
